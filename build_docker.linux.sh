4b0b8a8c8d985628e729b1d8a88982d85767601e
aizawan <aizawan@users.noreply.github.com>
2020-05-10 17:44:45 +0900

    add jupyter lab env.

--- build_docker.linux.sh
+++ build_docker.linux.sh
@@ -1,2 +1,3 @@
 docker build -t ubuntu.base -f ${PWD}/docker/base/Dockerfile .
 docker build -t ubuntu.conda -f ${PWD}/docker/conda/Dockerfile .
+docker build -t ubuntu.jupylab -f ${PWD}/docker/jupylab/Dockerfile .
